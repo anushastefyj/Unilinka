a74b2244c75d25e2cf94c509241c6871db9b1df3
anushastefyj <anushastefy002@gmail.com>
2026-08-27 22:07:33 +0530

    Remove outDir to use Vite's default 'dist' for Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,10 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
   build: {
-    outDir: "build",
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
